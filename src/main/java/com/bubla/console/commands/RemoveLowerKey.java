155077a67a0019d0faf5b4706f1a171aec816abb
ShablaBubla <rpervysin@gmail.com>
2024-09-19 16:40:28 +0300

    1.2

--- src/main/java/com/bubla/console/commands/RemoveLowerKey.java
+++ src/main/java/com/bubla/console/commands/RemoveLowerKey.java
@@ -12,7 +12,7 @@ public class RemoveLowerKey extends PrimeCommand<String>{
     public void execute(String args, Application application) {
         for (String key:
              application.getProducts().getProducts().keySet()) {
-            if(key.length() < args.length()){
+            if(key.compareTo(args)<0){
                 application.getProducts().remove(key);
             }
         }
